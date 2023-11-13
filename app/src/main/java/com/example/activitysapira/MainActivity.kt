dae4605a05a1857a1f67abea4e6cfbdae430f117
Dwiaprilia05 <adwi91088@gmail.com>
2023-11-13 10:23:49 +0700

    Initial commit

---
+++ app/src/main/java/com/example/activitysapira/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.example.activitysapira
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val button = findViewById<Button>(R.id.button)
+        button.setOnClickListener {
+            val Intent = Intent(  this, SecondActivity::class.java)
+            startActivity(Intent)
+        }
+
+    }
+}
